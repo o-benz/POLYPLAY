b9f036f8549cd4f8f3abe8b2ef2a7cac2cfc6d65
Omar Benzekri <omar.benzekri.2003@gmail.com>
2023-12-15 15:56:31 -0500

    Not done yet!

---
+++ site-web/src/components/Footer.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { NavLink, useLocation } from "react-router-dom";
+import Player from "./Player";
+
+export default function Footer() {
+  function loadPlayer(pathname) {
+    if (pathname.startsWith("/playlist")) {
+      return <Player />;
+    } else {
+      return (
+        <div>
+          <p>
+            Choisir une playlist à travers la <NavLink to="/index">Bibliothèque</NavLink> pour la faire jouer
+          </p>
+        </div>
+      );
+    }
+  }
+  return (
+    <footer id="playing-bar">
+      {loadPlayer(useLocation().pathname)}
+      <div id="creators">
+        <p>Omar Benzekri 2244082</p>
+        <p>Sidali Boukhezar 2183332</p>
+      </div>
+    </footer>
+  );
+}
