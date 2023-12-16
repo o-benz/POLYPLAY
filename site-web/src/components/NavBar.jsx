e592f00508f2a5f9ad8c4b5030b418f41e8d5061
Omar Benzekri <104547346+o-benz@users.noreply.github.com>
2023-12-16 01:18:37 -0500

    Update NavBar.jsx

--- site-web/src/components/NavBar.jsx
+++ site-web/src/components/NavBar.jsx
@@ -7,21 +7,18 @@ export default function NavBar() {
       <nav id="nav-bar" className="flex-column">
         <ul className="flex-column">
           <li>
-            {/*TODO : ajouter le lien de navigation vers la page /index */}
             <NavLink to="/index" className = {(navData) => (navData.isActive ? "active-page" : "none")}>
               <i className="fa fa-music"></i>
               <span>Ma Bibliothèque</span>
             </NavLink>
           </li>
           <li>
-            {/*TODO : ajouter le lien de navigation vers la page /create_playlist */}
             <NavLink to="/create_playlist" className = {(navData) => (navData.isActive ? "active-page" : "none")}>
               <i className="fa fa-plus"></i>
               <span>Créer Playlist</span>
             </NavLink>
           </li>
           <li>
-            {/*TODO : ajouter le lien de navigation vers la page /create_playlist */}
             <NavLink to="/about" className={(navData) => (navData.isActive ? "active-page" : "none")}>
               <i className="fa fa-info-circle"></i>
               <span>À Propos</span>
